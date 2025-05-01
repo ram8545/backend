652a670c01c1005eb974a193d5deb202bc113db3
Ram Sevak Mishra <ramsevakmishra69@gmail.com>
2025-05-01 11:07:06 +0530

    inital commit

--- server.js
+++ server.js
@@ -15,8 +15,14 @@ app.use(
     origin: "http://test.webwaps.in/", // Or '*' for all origins (development)
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true,
   })
 );
+
+app.options("*", cors()); // preflight support
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const validateEmail = (email) => {
   var re = /\S+@\S+\.\S+/;
