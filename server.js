619cb0bfac495942657b369707434d175b8c8fa8
Ram Sevak Mishra <ramsevakmishra69@gmail.com>
2025-05-01 10:53:56 +0530

    inital commit

--- server.js
+++ server.js
@@ -10,23 +10,11 @@ const port = 8000;
 const upload = multer();
 
 // CORS setup
-const allowedOrigins = [
-  "http://localhost:5173", // for development
-  "http://test.webwaps.in/", // for production
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    origin: "http://test.webwaps.in/", // Or '*' for all origins (development)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   })
 );
 
